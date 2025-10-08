4e300207e9bf890694ec2e1da1ec3f7f09b98038
Purshottam Manivel <purshottam_manivel@outlook.com>
2025-10-09 04:58:34 +0530

    implemented metadata

--- auth.ts
+++ auth.ts
@@ -3,7 +3,6 @@ import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
-import bcrypt from 'bcrypt';
 import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
@@ -27,13 +26,34 @@ export const { auth, signIn, signOut } = NextAuth({
                     .object({ email: z.string().email(), password: z.string().min(6) })
                     .safeParse(credentials);
 
-                if (parsedCredentials.success) {
-                    const { email, password } = parsedCredentials.data;
-                    const user = await getUser(email);
-                    if (!user) return null;
+                if (!parsedCredentials.success) {
+                    console.log('❌ Invalid input format');
+                    return null;
                 }
 
-                return null;
+                const { email, password } = parsedCredentials.data;
+                const user = await getUser(email);
+                if (!user) {
+                    console.log('❌ User not found:', email);
+                    return null;
+                }
+
+                console.log('✅ User found:', user.email);
+
+
+                // if (parsedCredentials.success) {
+
+                //     const { email, password } = parsedCredentials.data;
+                //     const user = await getUser(email);
+                //     if (!user) return null;
+                // }
+
+                if (password === user.password) {
+                    console.log('✅ password matched');
+                    return user;
+                }
+
+                return user;
             },
         }),
     ],
